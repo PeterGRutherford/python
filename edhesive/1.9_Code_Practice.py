0785d63982ebc88a9741a334f79b62e1f8846684
PeterGRutherford <54694350+PeterGRutherford@users.noreply.github.com>
2019-09-19 13:34:36 -0700

    Add files via upload

---
+++ edhesive/1.9_Code_Practice.py
@@ -0,0 +1,28 @@
+# 1.9_Code_Practice.py
+
+a = input("Enter an animal: ")
+s = input ("Enter a sound: ")
+
+e = str("E-I-E-I-O")
+
+print("Old Macdonald had a farm, " + e)
+print("And on his farm he had a " + a + ", " + e)
+print("With a " + s + "-" + s + " here and a " + s + "-" + s + " there")
+print("Here a "+ s + " there a " + s)
+print("Everywhere a " + s + "-" + s)
+print("Old Macdonald had a farm, " + e)
+
+'''
+coleman
+a = input("Enter an animal: ")
+s = input ("Enter a sound: ")
+e = "E-I-E-I-O"
+print ("Old Macdonald had a farm, " + e)
+print ("And on his farm he had a " + a + ", " + e)
+print ("With a " + s + "-" + s + " here and a " + s + "-" + s + " there")
+print ("Here a "+ s + " there a " + s)
+print ("Everywhere a "  + s + "-" + s )
+print ("Old Macdonald had a farm, " + e)
+
+
+'''
