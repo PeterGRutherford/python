b2ad177c612fc161b199c23bf4981d1b46b08b97
PeterGRutherford <54694350+PeterGRutherford@users.noreply.github.com>
2019-12-17 13:27:21 -0800

    Create Algorithm3.7.py

---
+++ Algorithm3.7.py
@@ -0,0 +1,14 @@
+
+a = input("vvvvvvvvvvvvvvvv\nEnter the name of any shape, I'll give you the number of sides \nMake sure it is capitalized EX: Square. not \"square\"\n^^^^^^^^^^^^^^^^\n\n")
+
+if(a == "Cube" and "Hexagon"):
+	print("6 sides")
+if(a == "Square" and "Rectangle" and "Tetrahedron" and "Rhombus" and "Parallelogram" and "Diamond" and "Trapezoid" and "Cuboid"):
+	print("4 sides")
+if(a == "Circle" and "Ellipse" and "Sphere" and "Oval"):
+	print("1 side")
+if(a == "Triangle" and "Cylinder"):
+	print("3 sides")
+if(a == "Pentagon" and "Triangular Prism"):
+	print("5 sides")
+
