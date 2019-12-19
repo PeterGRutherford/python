a6bbc9acf264598e748ed251bf2c865ab5a06fc7
PeterGRutherford <54694350+PeterGRutherford@users.noreply.github.com>
2019-12-18 22:02:54 -0800

    Update pig.py

--- pig.py
+++ pig.py
@@ -1,45 +1,5 @@
-#Reddit user "eggeggs". My code is below. I also had to edit this one to make it work  :]
-import random
-
-player_points = 0
-cpu_points = 0
-hold_player = 0
-hold_cpu = 0
-
-print("Welcome to PIG! \n RULES \n  If you roll '1': No new points, next player's turn \n  If you roll '2-6': either roll again 'r' or hold 'h' \n  If hold: sum of all rolls is added to score & turn passes to other players \n       !!!!FIRST TO 100 OR MORE POINTS WINS!!!!")
-
-print("It is your turn to roll the dice.")
-
-while player_points < 100 and cpu_points < 100: # while both are under 100
-    user_turn = input("Enter 'r' to roll or 'h' to hold: ")
-    if user_turn == 'r':
-        roll = random.randint(1,6)
-        if roll != 1:
-            hold_player += roll
-            print("You rolled:", roll ,"\nYour score:", hold_player)
-                continue
-        else:
-            print("You rolled:" + str(roll) + "\n" "0 points earned""\n""Current score:" + str(player_points))
-
-    else:
-        user_turn == 'h'
-        player_points += hold_player
-        print("You have chosen to hold \nYour score:" + str(player_points))
 
 
-while player_points < 100 and cpu_points < 100:
-    if hold_cpu < 20:
-        roll = random.randint(1,6)
-    if roll != 1:
-        hold_cpu += roll
-        print("CPU rolled:" + str(roll) + "\nCPU score:" + str(hold_player))
-            continue
-    else:
-        print("CPU rolled:" + str(roll) + "\n" "0 points earned""\n""Current score:" + str(player_points))
-        break
-'''
-This is my code. Although it doesn't work, I spent at least 6 hours or more on it
-I dont want to quit it but I have other finals, I understand the code above.
 
 import random
 round = 0
@@ -90,4 +50,3 @@ while(round != 10):
                 choice = "true"
             bot = bot + di
             print("Bot total: " + str(bot))
-'''
